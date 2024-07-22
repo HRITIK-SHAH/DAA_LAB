235b2a02c99a9976b521b4a8d6b485557bf972b4
HRITIK-SHAH <shahhritik214@gmail.com>
2024-07-22 23:58:12 +0530

    slargest

--- 1_Assignment/qno1.c
+++ 1_Assignment/qno1.c
@@ -1,7 +1,34 @@
+//Finding the  2nd smallest and 2nd largest elements
+
 #include<stdio.h>
 
-void main(){
-	printf("Hello\n");
-	printf("Hritik shah\n");
+int secondLargest(int arr[],int n){
+
+int largest = arr[0];
+int slargest = -1;
+
+for(int i=0; i<n; i++){
+
+ if(arr[i]>largest){
+ slargest =  largest;
+ largest = arr[i];
+ }
+ else{
+	 if(arr[i]<largest && arr[i] > slargest){
+		 slargest = arr[i];
+	 }
+ }
 }
+return slargest;
+}
+
+int main(){
+int arr[] = {1,2,4,7,7,5};
+int n = sizeof(arr) /sizeof(arr[0]);
+
+int slargest = secondLargest(arr,n);
+//int ssmallest = secondSmallest(arr,n);
+printf("%d",slargest);
+return 0;
+}	
 
