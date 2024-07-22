156cac03428414645c394333fe325e39d679641d
HRITIK-SHAH <shahhritik214@gmail.com>
2024-07-23 00:37:20 +0530

    2nd largest and smallest element in array

--- 1_Assignment/qno1.c
+++ 1_Assignment/qno1.c
@@ -3,32 +3,49 @@
 #include<stdio.h>
 
 int secondLargest(int arr[],int n){
-
-int largest = arr[0];
-int slargest = -1;
-
-for(int i=0; i<n; i++){
-
- if(arr[i]>largest){
- slargest =  largest;
- largest = arr[i];
- }
- else{
-	 if(arr[i]<largest && arr[i] > slargest){
-		 slargest = arr[i];
-	 }
- }
+     int largest = arr[0];
+     int slargest = -1;
+     
+     for(int i=0; i<n ; i++){
+         if(arr[i]> largest){
+             slargest = largest;
+             largest = arr[i];
+         }
+         
+         else {
+             if(arr[i]< largest && arr[i] > slargest)
+             slargest = arr[i];
+         }
+         
+     }
+     return slargest;
 }
-return slargest;
+
+int secondSmallest(int arr[], int n){
+    
+    int smallest = arr[0];
+    int ssmallest = 10000;
+    
+    for(int i=0 ; i<n ;i++){
+        if(arr[i]<smallest){
+            ssmallest = smallest;
+            smallest = arr[i];
+        }
+        
+        else{
+            if(arr[i] > smallest && arr[i] < ssmallest)
+            ssmallest = arr[i];
+        }
+    }
+    return ssmallest;
 }
 
 int main(){
-int arr[] = {1,2,4,7,7,5};
-int n = sizeof(arr) /sizeof(arr[0]);
-
-int slargest = secondLargest(arr,n);
-//int ssmallest = secondSmallest(arr,n);
-printf("%d",slargest);
-return 0;
+    int arr[] ={1,2,4,7,7,5};
+    int n =sizeof(arr) / sizeof(arr[0]);
+    int slargest = secondLargest(arr,n);
+    int ssmallest = secondSmallest(arr,n);
+    printf("The element in array are\n1,2,4,7,7,5\n");
+    printf("Second largest = %d \nSecond smallest = %d\n",slargest,ssmallest);
 }	
 
